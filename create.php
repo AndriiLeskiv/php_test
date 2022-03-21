70ac4185fb92e1f8c2a94a879e9aa81cf9f18c2b
AndriiLeskiv <leskivandrii21@gmail.com>
2022-03-21 09:04:37 +0200

    скуфеу ви сщииусе

--- create.php
+++ create.php
@@ -12,7 +12,7 @@ $currentgrade = $_POST['currentgrade'];
 $create_data = $_POST['data'];
 $online_search = $_POST['online_search'];
 
-mysqli_query($connect, "insert into information(id, profession, firstname, lastname, email, password, confirm school, currentgrade, data, online_search)
-value ('5', '$profession', '$firstname', '$lastname', '$email', '$password', '$confirm' '$shcool','$currentgrade', '$create_data' '$online_search')");
+mysqli_query($connect, "insert into information(id, profession, firstname, lastname, email, password, confirm, school, currentgrade, data, online_search)
+value (null, '$profession', '$firstname', '$lastname', '$email', '$password', '$confirm', '$shcool', '$currentgrade', '$create_data' '$online_search')");
 
 header('Location: ../index.php');
